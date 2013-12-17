d23981ebb7fa7cd92f781a2acdff4992d038ef46
Loic <llh1@sanger.ac.uk>
2013-12-17 16:04:50 +0000

    Decode gel aliquots

---
+++ lib/lims-warehousebuilder/decoders/aliquot_decoder_shared.rb
@@ -0,0 +1,41 @@
+require 'lims-warehousebuilder/models/aliquot'
+
+module Lims::WarehouseBuilder
+  module Decoder
+    module AliquotDecoderShared
+
+      # @param [Hash] aliquots
+      # @param [Hash] params
+      # @return [Array<Model::Aliquot>]
+      def decode_aliquots(aliquots, params={})
+        position = params["position"]
+
+        [].tap do |decoded_aliquots|
+          aliquots.each do |aliquot|
+            # if there is no sample associated to the aliquot, it usually 
+            # means the aliquot is the solvent
+            sample_uuid = aliquot["sample"]["uuid"] if aliquot["sample"]
+            aliquot_model = Model::Aliquot.new_for(@uuid, position, sample_uuid)
+            quantity, type = aliquot["quantity"], aliquot["type"]
+
+            # The concentration information is contained as an out_of_bounds parameter and not
+            # part of a stored aliquot in s2. As a result, when we update an aliquot, the 
+            # concentration might not be set and be nil. To keep it consistent in the warehouse,
+            # we set the concentration back from the warehouse model, so it's not updated to nil.
+            concentration = aliquot["out_of_bounds"] ? aliquot["out_of_bounds"]["concentration"] : aliquot_model.concentration
+
+            payload_aliquot_attributes = {
+              "container_uuid" => @uuid, "position" => position,
+              "sample_uuid" => sample_uuid, "quantity" => quantity,
+              "type" => type, "concentration" => concentration,
+              "date" => @date, "user" => @user
+            }
+
+            new_aliquot_model = map_attributes_to_model(aliquot_model, payload_aliquot_attributes)
+            decoded_aliquots << new_aliquot_model if new_aliquot_model
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/lims-warehousebuilder/decoders/gel_decoder.rb
@@ -0,0 +1,25 @@
+require 'lims-warehousebuilder/json_decoder'
+require 'lims-warehousebuilder/decoders/aliquot_decoder_shared'
+
+module Lims::WarehouseBuilder
+  module Decoder
+    class GelDecoder < JsonDecoder
+      include AliquotDecoderShared
+      
+      def _call(options)
+        [super, gel_aliquots]
+      end
+
+      private
+
+      # @return [Array<Model::Aliquot>]
+      def gel_aliquots
+        [].tap do |decoded_aliquots|
+          @payload["windows"].each do |position, aliquots|
+            decoded_aliquots << decode_aliquots(aliquots, "position" => position)
+          end
+        end
+      end
+    end
+  end
+end
